61449589e8f17b87d4eab0b8040f31ee9376a088
ChrisMagnuson <hourglasssand@gmail.com>
2016-09-19 17:03:57 -0400

    Initial commit

---
+++ TwilioTwimletPowerShell.psm1
@@ -0,0 +1,45 @@
+﻿$TwilioTwimletsRootURI = "http://twimlets.com"
+
+Function New-TwilioTwimletSimpleMenuURL {
+    param (
+        $Message,
+        $Options
+    )    
+    $URL = $TwilioTwimletsRootURI + "/menu?" + $(([Ordered]@{Message = $Message} + $Options) | ConvertTo-URLEncodedQueryStringParameterString)
+    $URL
+}
+
+Function New-TwilioTwimletEchoURL {
+    param (
+        $Twiml
+    )    
+    $URL = $TwilioTwimletsRootURI + "/echo?" + $($PSBoundParameters | ConvertTo-URLEncodedQueryStringParameterString)
+    $URL
+}
+
+
+Function New-TwilioTwimletSimpleMenuOption {
+    param (
+        $Digits,
+        $Url
+    )
+    [Ordered]@{
+        "Options[$Digits]" = $Url
+    }
+}
+
+Function New-TwilioTwimletMessageURL {
+    param (
+        [Parameter(Mandatory)][String[]]$Messages
+    )
+
+
+    ForEach ($Message in $Messages) {
+        $MessagesHashTable += [ordered]@{
+            "Message[$($Messages.IndexOf($Message))]" = $Message
+        }
+    }
+
+    $URL = $TwilioTwimletsRootURI + "/message?" + $($MessagesHashTable | ConvertTo-URLEncodedQueryStringParameterString)
+    $URL
+}
